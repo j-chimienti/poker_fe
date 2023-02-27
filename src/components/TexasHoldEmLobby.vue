e8d422abaa9aaac68fdc5f158dcc029b4343976b
Joe <joe.chimienti@eventdynamic.com>
2023-02-27 12:15:05 -0500

    add table info

--- src/components/TexasHoldEmLobby.vue
+++ src/components/TexasHoldEmLobby.vue
@@ -3,6 +3,8 @@
   <q-item v-for="table in tables" :key="table">
     <q-btn  @click="$router.push({name: 'table', params: {id: table.id}})">
     <q-chip>{{table.title}}</q-chip>
+    <q-chip>round:{{table.round}}</q-chip>
+    <q-chip>hand:{{table.hand}}</q-chip>
     <q-chip>{{Object.keys(table.pokerPlayers).length}} players</q-chip>
     </q-btn>
   </q-item>
