0eb44b5dae278bc3e26ca9f583c1af5d5d65d97c
Joe <joe.chimienti@eventdynamic.com>
2023-03-05 08:00:52 -0500

    remove ws notification

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,18 +12,15 @@
         />
         <img :src="logoSrc" style="width: 220px;" class="q-mr-lg" />
       </div>
-<!--      <WebsocketConnectionStatus/>-->
     </q-toolbar>
   </q-header>
 </template>
 <script>
 import StoreMixin from "../mixins/StoreMixin.vue"
 import logoSrc from "../assets/bj-logo.png"
-import WebsocketConnectionStatus from "components/WebsocketConnectionStatus.vue";
 
 export default {
   name: "NavBar",
-  components: {WebsocketConnectionStatus},
   data() {
     return {
       logoSrc,
