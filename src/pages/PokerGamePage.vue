92b3c093ff0dd83304fa73a643c141c539baf085
Joe <joe.chimienti@eventdynamic.com>
2023-03-13 08:31:50 -0400

    only show if table present

--- src/pages/PokerGamePage.vue
+++ src/pages/PokerGamePage.vue
@@ -1,46 +1,49 @@
 <template>
-  <div class="row justify-center">
-    <q-btn @click="navigateToLobby" label="lobby" icon="arrow_left" color="secondary" />
-    <div>
-      <div v-if="table">
-        <q-chip>{{table.title}}</q-chip>
-        <q-chip>{{table.round}}</q-chip>
-        <q-chip>hand:{{table.hand}}</q-chip>
-        <q-chip>pot:{{table.pot}}</q-chip>
-        <q-chip>players:{{Object.keys(table.pokerPlayers).length}}</q-chip>
-        <div  v-if="countdownText > 0">
-          next game starts:
-          <q-knob
-            :min="0"
-            :max="15"
-            v-model="countdownText"
-            size="50px"
-            :thickness="0.22"
-            class="q-ma-md"
-            show-value
-          />
+  <div v-if="!table">loading...</div>
+  <div v-else>
+    <div class="row justify-center">
+      <q-btn @click="navigateToLobby" label="lobby" icon="arrow_left" color="secondary" />
+      <div>
+        <div >
+          <q-chip>{{table.title}}</q-chip>
+          <q-chip>{{table.round}}</q-chip>
+          <q-chip>hand:{{table.hand}}</q-chip>
+          <q-chip>pot:{{table.pot}}</q-chip>
+          <q-chip>players:{{Object.keys(table.pokerPlayers).length}}</q-chip>
+          <div  v-if="countdownText > 0">
+            next game starts:
+            <q-knob
+              :min="0"
+              :max="15"
+              v-model="countdownText"
+              size="50px"
+              :thickness="0.22"
+              class="q-ma-md"
+              show-value
+            />
+          </div>
+
+
         </div>
-
-
       </div>
     </div>
-  </div>
-  <div>
-    <PokerTable />
-    <div class="row justify-center" v-if="joinedTable && table && table.hand && table.hand !== 'WAITING'" >
-      <q-form @submit="onRaise">
-        <q-input type="number" v-model.number="raiseAmount"/>
-        <q-btn type="submit"  color="red" label="raise"/>
-      </q-form>
-    <q-btn-group>
+    <div>
+      <PokerTable />
+      <div class="row justify-center" v-if="joinedTable && table && table.hand && table.hand !== 'WAITING'" >
+        <q-form @submit="onRaise">
+          <q-input type="number" v-model.number="raiseAmount"/>
+          <q-btn type="submit"  color="red" label="raise"/>
+        </q-form>
+        <q-btn-group>
 
-      <q-btn @click="call(table.id)" color="blue" label="call"/>
-      <q-btn @click="fold(table.id)" color="grey" label="fold"/>
-    </q-btn-group>
+          <q-btn @click="call(table.id)" color="blue" label="call"/>
+          <q-btn @click="fold(table.id)" color="grey" label="fold"/>
+        </q-btn-group>
+      </div>
     </div>
+    <q-btn v-if="joinedTable" @click="onLeave(table.id)" color="red" label="leave"/>
+    <q-btn v-else size="lg" @click="join(table.id)" color="green" label="join"/>
   </div>
-  <q-btn v-if="joinedTable" @click="onLeave(table.id)" color="red" label="leave"/>
-  <q-btn v-else size="lg" @click="join(table.id)" color="green" label="join"/>
 </template>
 
 <script>
